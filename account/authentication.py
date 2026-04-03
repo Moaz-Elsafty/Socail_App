21eef2b7f402a70174c98a3fd9b0138df71b8bd0
Moaz Elsafty <moaz.elsafty11@gmail.com>
2026-04-03 18:01:43 +0200

    create custom backend authentication & make user's email unique

---
+++ account/authentication.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+
+class EmailAuthBackend:
+    """
+    Authenticate using an e-mail address.
+    """
+    def authenticate(self, request, username=None, password=None):
+        if not username or not password :
+            return None
+
+        users = User.objects.filter(Q(email__iexact=username) | Q(username=username))
+
+        if users.count() > 1:
+            return None
+        
+        user = users.first()
+
+        if user and user.check_password(password):
+            return user
+        
+        return None
+
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
