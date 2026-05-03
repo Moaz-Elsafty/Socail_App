0daafcb26d267901a42ea6d38212c7575e205536
Moaz Elsafty <moaz.elsafty11@gmail.com>
2026-05-03 09:35:58 +0300

    fix views fiel where print was used

--- images/views.py
+++ images/views.py
@@ -49,10 +49,8 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
-                print(image.users_like.all())
             else:
                 image.users_like.remove(request.user)
-                print(image.users_like.all())
 
             return JsonResponse({'status': 'ok'})
         except Image.DoesNotExist:
