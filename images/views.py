5eeaaa3b57ede84bcff2f1987e5649f6564a408d
Moaz Elsafty <moaz.magdy@dphish.com>
2026-08-21 23:00:03 +0530

    feat: add list view for images

--- images/views.py
+++ images/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import JsonResponse, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
@@ -10,7 +11,6 @@ from .models import Image
 @login_required
 def image_create(request):
     if request.method == 'POST':
-        print(request.POST)
         form = ImageCreateForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -41,7 +41,6 @@ def image_detail(request, id, slug):
 @login_required
 @require_POST
 def image_like(request):
-    print(request.POST)
     image_id = request.POST.get('id')
     action = request.POST.get('action')
     if image_id and action:
@@ -57,4 +56,32 @@ def image_like(request):
             pass
 
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
         
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
